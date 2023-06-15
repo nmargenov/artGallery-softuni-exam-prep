f64778b1177d1a0f828b5de75e817466948fcb70
nmargenov <nikolay_margenov@yahoo.com>
2023-06-15 21:32:31 +0300

    home works with db

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,16 @@
+const { getAllPublications } = require('../managers/publicationManager');
+
 const router = require('express').Router();
 
-router.get(['/','/index'],(req,res)=>{
-    res.status(302).render('home');
+router.get(['/','/index'],async(req,res)=>{
+    try{
+        const publications = await getAllPublications().lean();
+        const hasPublications = publications.length>0;
+        console.log(publications);
+        res.status(302).render('home',{publications,hasPublications});
+    }catch(err){
+
+    }
 });
 
 module.exports = router;
